1750ed216b97f00581dfbd19d2d5144f23398ff9
geniocoins <85482753+geniocoins@users.noreply.github.com>
2021-06-07 19:11:54 +0700

    Update Home.tsx

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -91,9 +91,9 @@ const Home: React.FC = () => {
     <Page>
       <Hero>
         <Heading as="h1" scale="xl" mb="24px" color="secondary">
-          {t('PancakeSwap')}
+          {t('GeniocoinsSwap')}
         </Heading>
-        <Text>{t('The #1 AMM and yield farm on Binance Smart Chain.')}</Text>
+        <Text>{t('The Next #1 AMM and yield farm on Binance Smart Chain.')}</Text>
       </Hero>
       <div>
         <Cards>
